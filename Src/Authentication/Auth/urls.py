106360ff06a3f7d9e97bea2ba8729344b09c7ccf
Wajid Korkani <errormrcoder@gmail.com>
2024-07-18 08:59:53 +0500

    View is created for home page with url and also base.html templates is created so all other templates can extends on it and index.html tempaltes is created for the home page.

--- Src/Authentication/Auth/urls.py
+++ Src/Authentication/Auth/urls.py
@@ -3,4 +3,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
 urlpatterns = [
+    path('', Home, name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Src/Authentication/Auth/views.py
+++ Src/Authentication/Auth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+    template = 'index.html'
+    context = {
+        'Text' : 'Hello django!'
+    }
+    return render(request, template, context)
