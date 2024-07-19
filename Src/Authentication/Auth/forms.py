0e58574dd7e3fb3a66f6d046397fa0be32f469a8
Wajid Korkani <errormrcoder@gmail.com>
2024-07-19 09:31:38 +0500

    Authentication system is complete.

---
+++ Src/Authentication/Auth/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name' )  # Remove username, email, password fields
